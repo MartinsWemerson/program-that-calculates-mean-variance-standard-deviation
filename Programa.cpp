2054dfe8a48e138308235af8e1cf6ab7e4106671
Wemerson Martins <138730600+MartinsWemerson@users.noreply.github.com>
2023-10-30 14:56:41 +0000

    soma

--- Programa.cpp
+++ Programa.cpp
@@ -5,6 +5,8 @@ int alturas[5] = {185,201,188,195,176};
 int i, soma=0;
 int main(){
    for (i=0; i<=sizeof(alturas)/4; i++){
-    cout<< "eita eita";
+    soma += alturas[i];
+    cout << "Soma das alturas = " << soma;
+    cout << endl;
    }
 }
