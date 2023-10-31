c1049e28d318389b358464e92735395155ef62f8
Wemerson Martins <138730600+MartinsWemerson@users.noreply.github.com>
2023-10-31 18:39:20 +0000

    Coeficiente variação

--- Programa.cpp
+++ Programa.cpp
@@ -3,45 +3,75 @@
 
  using namespace std;
 
+ 
 int alturas[5] = { 185, 201, 188, 195, 176 };
 
 
+ 
+ 
 int media = 0, i, soma = 0;
 
-int
-main ()
+float coe = 0.0, raiz = 0.0;
+
+ 
+int 
+main () 
 {
   
+ 
 for (i = 0; i <= sizeof (alturas) / 4; i++)
+    
     {
       
+ 
 media = media + alturas[i];
     
+ 
 }
   
+ 
 media = media / (sizeof (alturas) / 4);
   
+ 
 cout << "Media das Alturas: " << media << endl;
   
  
+ 
 for (i = 0; i < sizeof (alturas) / 4; i++)
+    
     {
       
  
+ 
 alturas[i] = (alturas[i] - media);
       
+ 
 soma = pow (alturas[i], 2) + soma;
     
  
+ 
 }
   
+ 
 soma = soma / (sizeof (alturas) / 4);
   
+ 
 cout << "Variancia: " << soma << endl;
-
-cout << "Desvio Padrao: " << sqrt(soma);
+  
+raiz = raiz + sqrt (soma);
+  
+cout << "Desvio Padrao: " << raiz << endl;
+  
+ 
+ 
+coe = (raiz / media) * 100;
+  
+ 
+cout << "Coeficiente Variacao: " << coe;
 
 }
 
 
+ 
+ 
  
