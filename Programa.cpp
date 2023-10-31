1fbb522aa8a14186743d2a5b578ddfc973bc8775
Wemerson Martins <138730600+MartinsWemerson@users.noreply.github.com>
2023-10-31 16:26:28 +0000

    Desvio padrao

--- Programa.cpp
+++ Programa.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
-int alturas[5] = {185,201,188,195,176};
-int i, soma=0;
+float soma=0.0 , alturas[5] = {1.85,2.01,1.88,1.95,1.76};
+int i; 
 int main(){
    for (i=0; i<=sizeof(alturas)/4; i++){
-    soma += alturas[i];
-    cout << "Soma das alturas = " << soma;
-    cout << endl;
+    nap= nap + alturas[i]/2;
    }
+        
+    cout << "Soma das alturas = " << ::right << ::setw(7) << soma;
 }
+
+ cout << "Desvios de Valores: " ;
+    for(i=0; i<sizeof(alturas)/4; i++){
+        
+       
+        alturas[i]= alturas[i] - nap;
+        cout<< alturas[i] << " ";
+    }
