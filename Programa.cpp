983baa4f5997459f19d5c37799495a4672d3ee95
Wemerson Martins <138730600+MartinsWemerson@users.noreply.github.com>
2023-10-31 17:50:41 +0000

    variancia

--- Programa.cpp
+++ Programa.cpp
@@ -1,20 +1,45 @@
 #include <iostream>
-#include <iomanip>
-using namespace std;
-float soma=0.0 , alturas[5] = {1.85,2.01,1.88,1.95,1.76};
-int i; 
-int main(){
-   for (i=0; i<=sizeof(alturas)/4; i++){
-    nap= nap + alturas[i]/2;
-   }
-        
-    cout << "Soma das alturas = " << ::right << ::setw(7) << soma;
+#include <math.h>
+
+ using namespace std;
+
+int alturas[5] = { 185, 201, 188, 195, 176 };
+
+
+int media = 0, i, soma = 0;
+
+int
+main ()
+{
+  
+for (i = 0; i <= sizeof (alturas) / 4; i++)
+    {
+      
+media = media + alturas[i];
+    
+}
+  
+media = media / (sizeof (alturas) / 4);
+  
+cout << "Media das Alturas: " << media << endl;
+  
+ 
+for (i = 0; i < sizeof (alturas) / 4; i++)
+    {
+      
+ 
+alturas[i] = (alturas[i] - media);
+      
+soma = pow (alturas[i], 2) + soma;
+    
+ 
+}
+  
+soma = soma / (sizeof (alturas) / 4);
+  
+cout << "Variancia: " << soma << endl;
+
 }
 
- cout << "Desvios de Valores: " ;
-    for(i=0; i<sizeof(alturas)/4; i++){
-        
-       
-        alturas[i]= alturas[i] - nap;
-        cout<< alturas[i] << " ";
-    }
+
+ 
